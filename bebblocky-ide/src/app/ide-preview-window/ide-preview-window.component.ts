91adc66231dae2fa3bb7ed7d152f9bb7170cbbe1
ffekirnew <ffekirnew0808@gmail.com>
2023-05-05 11:23:04 +0300

    added code to implement and bind the preview section

--- bebblocky-ide/src/app/ide-preview-window/ide-preview-window.component.ts
+++ bebblocky-ide/src/app/ide-preview-window/ide-preview-window.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CodeService } from '../services/code.service';
 
 @Component({
   selector: 'app-ide-preview-window',
   templateUrl: './ide-preview-window.component.html',
   styleUrls: ['./ide-preview-window.component.css']
 })
-export class IdePreviewWindowComponent {
+export class IdePreviewWindowComponent implements OnInit {
 
+  @ViewChild('formattedHtml') formattedHtml!: ElementRef;
+  
+  constructor(
+    private codeService: CodeService
+    ) {}
+    
+  ngOnInit() {
+    this.codeService.outputSubject.subscribe((output) => {
+      this.formattedHtml.nativeElement.innerHTML = output;
+    });
+  }
 }

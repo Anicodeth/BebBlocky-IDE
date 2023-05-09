6110662d86c5e02b143b5b8cdb3217e83ee377e3
ffekirnew <ffekirnew0808@gmail.com>
2023-05-09 17:06:35 +0300

    started working on the profile section part

--- bebblocky-ide/src/app/profile-page-sidebar/profile-page-sidebar.component.ts
+++ bebblocky-ide/src/app/profile-page-sidebar/profile-page-sidebar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faTachometerAlt, faCode, faUser, faCog, faTableCells } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile-page-sidebar',
   templateUrl: './profile-page-sidebar.component.html',
-  styleUrls: ['./profile-page-sidebar.component.css']
+  styleUrls: ['./profile-page-sidebar.component.scss']
 })
-export class ProfilePageSidebarComponent {
+export class ProfilePageSidebarComponent implements OnInit {
+  isExpanded: boolean = false;
+  menuItems: any[] = [
+    { name: 'Code', link: '/ide', icon: faCode, class: 'special' },
+    { name: 'Dashboard', link: '/dashboard', icon: faTachometerAlt, class: 'regular' },
+    { name: 'Profile', link: '/profile', icon: faUser, class: 'regular' },
+    { name: 'Settings', link: '/settings', icon: faCog, class: 'regular' },
+    { name: 'Catalog', link: '/catalog', icon: faTableCells, class: 'regular' },
+    // Add more menu items here
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  expandSidebar(): void {
+    this.isExpanded = true;
+  }
+
+  shrinkSidebar(): void {
+    this.isExpanded = false;
+  }
 }

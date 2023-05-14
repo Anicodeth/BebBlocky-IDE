735e8c0314e263f9806b89201c9b849be1e68c4a
Ananya <afmtrendaid@gmail.com>
2023-05-14 08:01:42 -0700

    port outlet

--- bebblocky-api/index.js
+++ bebblocky-api/index.js
@@ -73,7 +73,7 @@ app.post('/signin', async (req, res)=>{
 });
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
-    console.log("Listening on port 3000");
+    console.log("Listening on port");
 });
 
 

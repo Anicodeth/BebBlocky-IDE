b56f78665895bd45110c3553aba08b2ad05ff68e
Ananya <afmtrendaid@gmail.com>
2023-05-14 10:03:45 -0700

    port outlet

--- bebblocky-api/index.js
+++ bebblocky-api/index.js
@@ -5,7 +5,38 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 //Models
-const  { User }  = require('./Models/user');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  progress: [{
+    topic: {
+      type: String,
+      required: true
+    },
+    completedPercent: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }]
+});
+
+
+const User = mongoose.model('User', userSchema);
 
 
 app = express();

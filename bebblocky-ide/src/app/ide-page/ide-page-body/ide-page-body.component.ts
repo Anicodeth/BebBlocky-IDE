3faff4a80bf873c76c7fa2b430fd0cec977886eb
Ananya <afmtrendaid@gmail.com>
2023-07-08 15:33:15 -0700

    phone container

--- bebblocky-ide/src/app/ide-page/ide-page-body/ide-page-body.component.ts
+++ bebblocky-ide/src/app/ide-page/ide-page-body/ide-page-body.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./ide-page-body.component.css']
 })
 export class IdePageBodyComponent {
+  public activeWindow: 'slide' | 'editor' | 'preview' = 'slide';
 
+
+
+  public addSlideClass: boolean = true;
+  public addEditorClass: boolean = false;
+  public addPreviewClass: boolean = false;
+
+
+  toggleWindows(window: 'slide' | 'editor' | 'preview'): void {
+    this.activeWindow = window;
+    if (window === 'slide') {
+      this.addSlideClass = true;
+      this.addEditorClass = false;
+      this.addPreviewClass = false;
+    } else if (window === 'editor') {
+      this.addSlideClass = false;
+      this.addEditorClass = true;
+      this.addPreviewClass = false;
+    }
+    else if (window === 'preview') {
+      this.addSlideClass = false;
+      this.addEditorClass = false;
+      this.addPreviewClass = true;
+    }
+    
+  }
 }

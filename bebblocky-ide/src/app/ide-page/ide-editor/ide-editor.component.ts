291ca157f4a1e80a9104525779f3db22e25878ef
Ananya <afmtrendaid@gmail.com>
2023-07-08 04:40:35 -0700

    Code pen model

--- bebblocky-ide/src/app/ide-page/ide-editor/ide-editor.component.ts
+++ bebblocky-ide/src/app/ide-page/ide-editor/ide-editor.component.ts
@@ -11,49 +11,118 @@ import { CodeEditorService } from '../../shared/services/code-editor.service';
 })
 export class IdeEditorComponent implements AfterViewInit, OnChanges {
 
-  @ViewChild("editor") private editor: ElementRef<HTMLElement> | any;
+  @ViewChild("htmlEditor") private htmlEditor: ElementRef<HTMLElement> | any;
+  @ViewChild("cssEditor") private cssEditor: ElementRef<HTMLElement> | any;
+  @ViewChild("jsEditor") private jsEditor: ElementRef<HTMLElement> | any;
+  public activeEditor: 'html' | 'css' | 'js' = 'html';
+
+  public htmlCode: string = '';
+  public cssCode: string = '';
+  public jsCode: string = ''; 
+
+  public compiledCode = `
+      <html>
+      <head>
+        <style>${this.cssCode}</style>
+      </head>
+      <body>
+        ${this.htmlCode}
+        <script>${this.jsCode}</script>
+      </body>
+      </html>
+    `;
+
 
   constructor(
     private codeEditorService: CodeEditorService,
   ) { }
 
+    toggleEditor(editor: 'html' | 'css' | 'js'): void {
+    this.activeEditor = editor;
+  }
+  compileCode(code:string): void {
+    console.log(code);    
+    this.codeEditorService.userCode.next(code);
+  }
+  get structuredCode(): any {
+    return {
+      code: `
+        <html>
+        <head>
+          <style>${this.cssCode}</style>
+        </head>
+        <body>
+          ${this.htmlCode}
+          <script>${this.jsCode}</script>
+        </body>
+        </html>
+      `,
+      // Add other properties as needed
+    };
+  }
+
   ngAfterViewInit(): void {
     this.codeEditorService.fontSize.subscribe((fontSize) => {
-      aceEditor.setFontSize(fontSize);
+      aceHtmlEditor.setFontSize(fontSize);
+      aceCssEditor.setFontSize(fontSize);
+      aceJsEditor.setFontSize(fontSize);
     });
-    const aceEditor = ace.edit(this.editor.nativeElement);
+    const aceHtmlEditor = ace.edit(this.htmlEditor.nativeElement);
+    const aceCssEditor = ace.edit(this.cssEditor.nativeElement);
+    const aceJsEditor = ace.edit(this.jsEditor.nativeElement);
 
     this.codeEditorService.editorTheme.subscribe((theme) => {
-      aceEditor.setTheme(`ace/theme/${theme}`);
+
+      aceHtmlEditor.setTheme(`ace/theme/${theme}`);
+      aceCssEditor.setTheme(`ace/theme/${theme}`);
+      aceJsEditor.setTheme(`ace/theme/${theme}`);
     });
 
     ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict');
 
-   
-    aceEditor.session.on('change', () => {
-      this.codeEditorService.userCode.next(aceEditor.getValue());
+
+    aceHtmlEditor.session.on('change', () => {
+      this.htmlCode = aceHtmlEditor.getValue();
+      console.log(this.htmlCode);
+      this.compileCode(this.structuredCode.code);
+    });
+
+    aceCssEditor.session.on('change', () => {
+      this.cssCode = aceCssEditor.getValue();
+      this.compileCode(this.structuredCode.code);
+    });
+    
+    aceJsEditor.session.on('change', () => {
+      this.jsCode = aceJsEditor.getValue();
+
+      this.compileCode(this.structuredCode.code);
     });
 
     this.codeEditorService.startCode.subscribe((startCode) => {
-      console.log('here', startCode);
-      aceEditor.session.setValue(startCode);
+      aceHtmlEditor.session.setValue(startCode);
     });
 
-    this.codeEditorService.editorLanguage.subscribe((language) => {
-      aceEditor.session.setMode(`ace/mode/${language}`);
-    });
+      aceHtmlEditor.session.setMode(`ace/mode/html`);
+      aceCssEditor.session.setMode(`ace/mode/css`);
+      aceJsEditor.session.setMode(`ace/mode/javascript`);
 
-    aceEditor.setFontSize(18);
-    aceEditor.setTheme("ace/theme/cobalt");
-    aceEditor.setOptions({wrapBehavioursEnabled: false
-    });
+
+      aceHtmlEditor.setFontSize(18);
+      aceHtmlEditor.setTheme("ace/theme/cobalt");
+      aceHtmlEditor.setOptions({wrapBehavioursEnabled: false});
+
+      aceCssEditor.setFontSize(18);
+      aceCssEditor.setTheme("ace/theme/cobalt");
+      aceCssEditor.setOptions({wrapBehavioursEnabled: false});
+
+      aceJsEditor.setFontSize(18);
+      aceJsEditor.setTheme("ace/theme/cobalt");
+      aceJsEditor.setOptions({wrapBehavioursEnabled: false});
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     ace.config.set("fontSize", `${this.codeEditorService.fontSize}px`);
-
   }
-
 
 }
 

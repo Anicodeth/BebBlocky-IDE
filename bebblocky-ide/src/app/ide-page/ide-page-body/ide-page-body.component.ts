14de3a117a44ce270da31140deffa9b5fce1afd8
Ananya <afmtrendaid@gmail.com>
2023-07-16 01:05:23 -0700

    python trial 2

--- bebblocky-ide/src/app/ide-page/ide-page-body/ide-page-body.component.ts
+++ bebblocky-ide/src/app/ide-page/ide-page-body/ide-page-body.component.ts
@@ -12,7 +12,7 @@ import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 export class IdePageBodyComponent implements OnInit {
 
   public activeWindow: 'slide' | 'editor' | 'preview' = 'slide';
-  public type: string = 'python'
+  public type: string = 'html'
 
 
 

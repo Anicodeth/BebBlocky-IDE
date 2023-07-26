0d4ad5d038d6e233d705fb69def46591f9b211b4
ffekirnew <ffekirnew0808@gmail.com>
2023-07-26 10:44:56 +0300

    Add salting, hasing, custom error handling in the backend

--- bebblocky-api/models/User.js
+++ bebblocky-api/models/User.js
@@ -21,16 +21,28 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    minlength: [3, 'Username must be at least 3 characters long']
+  },
+  salt: {
+    type: String,
+    required: true
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    validate: {
+      validator: function (value) {
+        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+      },
+      message: props => `${props.value} is not a valid email address!`
+    }
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: [8, 'Password must be at least 8 characters long']
   },
   role: {
       type: String,
--- bebblocky-api/services/authService.js
+++ bebblocky-api/services/authService.js
@@ -1,28 +1,89 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
+const BadRequestError = require('../errors/BadRequestError');
+const NotFoundError = require('../errors/NotFoundError');
+const ForbiddenError = require('../errors/ForbiddenError');
+const InternalServerError = require('../errors/InternalServerError');
+
+async function hashPassword(salt, password) {
+  return await bcrypt.hash(password, salt);
+}
+
+async function generateSalt() {
+  return await bcrypt.genSalt(10);
+}
+
+async function validatePassword(salt, plainPassword, hashedPassword) {
+  const hash = await bcrypt.hash(plainPassword, salt);
+  return hash === hashedPassword;
+}
+
+
+async function asyncWrapper(callback) {
+  try {
+    return await callback();
+  } catch (error) {
+    if (error.name == 'MongoError') {
+      if (error.code == 11000)
+        throw new BadRequestError('User already exists.');
+      else if (error.code == 2)
+        throw new NotFoundError('User not found.');
+      else
+        throw new InternalServerError(error.message);
+    } else {
+      throw error;
+    }
+  }
+}
 
 exports.createUser = async (username, email, password) => {
-  const user = new User({ username, email, password });
-  await user.save();
-  return user;
+  return await asyncWrapper(async () => {
+    // try and see if any other user exists with the same username
+    let existingUser = User.find({ username });
+    if (existingUser) {
+      throw new BadRequestError('Another user exists with the same username.');
+    }
+
+    // try and see if any other user exists with the same username
+    existingUser = User.find({ email });
+    if (existingUser) {
+      throw new BadRequestError('Another user exists with the same email.');
+    }
+
+    const salt = await generateSalt();
+    password = await hashPassword(salt, password);
+
+    console.log('creating a new user: controller', username, email, password, salt);
+
+    const user = new User({ username, salt, email, password });
+
+    return await user.save();
+  });
 };
 
 exports.getUserByUsername = async (username) => {
-  const user = await User.findOne({ username });
-  return user;
-};
-
-exports.checkExistingUser = async (username, email) => {
-  const user = await User.findOne({ $or: [{ username }, { email }] });
-  return user;
+  return await asyncWrapper(async () => {
+    return await User.findOne({ username });
+  });
 };
 
 exports.loginUser = async (username, password) => {
-  const user = await User.findOne({ username });
-  if (!user || user.password !== password) {
-    throw new Error('Invalid username or password');
-  }
+  return await asyncWrapper(async () => {
+    const user = await User.findOne({ username });
 
-  const token = jwt.sign({ userId: user._id }, 'Ananya');
-  return { user, token };
+    if (!user) {
+      throw new BadRequestError('Invalid username or password.');
+    }
+
+    const isValid = await validatePassword(user.salt, password, user.password);
+    if (!isValid) {
+      throw new BadRequestError('Invalid username or password.');
+    }
+
+    const token = jwt.sign({ userId: user._id }, 'Ananya');
+    return { user, token };
+  });
 };

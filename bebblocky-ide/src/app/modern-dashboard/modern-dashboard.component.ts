f632e0642276ca04f81b160d02735acfc5e7767f
ffekirnew <ffekirnew0808@gmail.com>
2023-07-26 08:17:00 +0300

    Add the modern component

---
+++ bebblocky-ide/src/app/modern-dashboard/modern-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modern-dashboard',
+  templateUrl: './modern-dashboard.component.html',
+  styleUrls: ['./modern-dashboard.component.css']
+})
+export class ModernDashboardComponent {
+
+}

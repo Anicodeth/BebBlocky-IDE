07df5b8955ec6946496fbfb7fc248193b707520a
ffekirnew <ffekirnew0808@gmail.com>
2023-07-27 20:21:13 +0300

    Fixed bug

--- bebblocky-api/middlewares/authMiddleware.js
+++ bebblocky-api/middlewares/authMiddleware.js
@@ -3,15 +3,15 @@ const userService = require('../services/userService');
 
 module.exports = async (req, res, next) => {
   if (!req.header('Authorization')) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: 'Authorization header is missing.' });
   }
 
   const token = req.header('Authorization').replace('Bearer ', '');
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: 'Authorization token is missing.' });
   }
 
-  jwt.verify(token,process.env.SECURITYKEY || 'Ananya', async (err, decoded) => {
+  jwt.verify(token, process.env.SECURITYKEY || 'Ananya', async (err, decoded) => {
     if (err) {
       return res.status(403).json({ message: 'Invalid token' });
     }

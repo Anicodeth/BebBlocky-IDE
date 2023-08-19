d3f1a5fdc8b56af45314c92f3802680641b1a43a
ffekirnew <ffekirnew0808@gmail.com>
2023-08-19 17:46:14 +0300

    feat(demo-dash-board): Fetch and render courses on the new dashboard

--- dash_demo-main/dash_demo-main/src/pages/courses.tsx
+++ dash_demo-main/dash_demo-main/src/pages/courses.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { ArrowRight, Star, StarHalf } from "lucide-react";
@@ -7,11 +8,58 @@ import {
     CardContent,
     CardFooter,
     CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge";
 import SearchInput from "@/components/search-input";
 
+import htmlLogo from '../../public/icons/html5/html5-original.svg'
+import cssLogo from '../../public/icons/css3/css3-original.svg'
+import jsLogo from '../../public/icons/javascript/javascript-original.svg'
+interface LogoDict {
+    [key: string]: string; // Assuming the imported SVG paths are strings
+}
+
+const logoDict: LogoDict = {
+    "html": htmlLogo,
+    "css": cssLogo,
+    "js": jsLogo,
+}
+
+interface Slide {
+  _id: string
+  backgroundColor: string
+  color: string
+  title: string
+  titleFont: string
+  content: string
+  contentFont: string
+  code: string
+  startingCode: string
+  image: string
+}
+
+interface Course {
+  _id: number
+  courseId: number
+  courseTitle: string
+  courseDescription: string
+  courseLanguage: string
+  slides: Slide[]
+}
+
 export default function CoursesRoute() {
+    const [courses, setCourses] = useState<Course[]>([]);
+
+    useEffect(() => {
+    fetch("https://beb-blocky-ide.vercel.app/api/v1/courses")
+      .then(response => response.json())
+      .then(data => {
+        setCourses(data.courses)
+      })
+      .catch(error => console.error("Error fetching courses:", error));
+    }, []);
+
     return (
         <>
             <Head>
@@ -28,17 +76,19 @@ export default function CoursesRoute() {
                 </p>
                 <SearchInput />
                 <hr className="w-full border-gray-300 mb-4" />
-                <h2 className="text-2xl font-bold tracking-tight">Most Popular Courses</h2>
+        { courses.length == 0 && <p>Sorry, we couldn't load our courses due to errors. Try again later.</p>}
+        { courses.length > 0 && <h2 className="text-2xl font-bold tracking-tight">Most Popular Courses</h2> }
                 <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 grid-cols-1 items-center gap-4 pb-4 pt-2">
+                { courses.map(course => (
                     <Card className="text-dark-ebony">
                         <CardHeader>
-                            <Image src="https://via.placeholder.com/220x150 " alt="Course image" width={220} height={150} className="mx-auto rounded-xl border border-apple mb-3" />
+                            <Image src={logoDict[course.courseLanguage]!} alt="Course image" width={180} height={110} className="mx-auto rounded-xl mb-3" />
                             <div className="flex flex-row justify-center gap-2">
-                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">HTML</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">CSS</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">JS</Badge>
-                            </div>
+                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">{course.courseLanguage.toUpperCase()}</Badge>                            </div>
                         </CardHeader>
                         <CardContent className="-mt-5">
-                            <p className="text-2xl font-semibold">Web Development Course</p>
+                            <CardTitle>{course.courseTitle}</CardTitle>
+                            <p className="text-2xl font-semibold"></p>
                             <div className="flex flex-row justify-between items-center mt-2">
                                 <div className="flex gap-1">
                                     <Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><StarHalf className="text-apple" size={16} />
@@ -53,128 +103,9 @@ export default function CoursesRoute() {
                             </div>
                         </CardFooter>
                     </Card>
-                    <Card className="text-dark-ebony">
-                        <CardHeader>
-                            <Image src="https://via.placeholder.com/220x150 " alt="Course image" width={220} height={150} className="mx-auto rounded-xl border border-apple mb-3" />
-                            <div className="flex flex-row justify-center gap-2">
-                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">HTML</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">CSS</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">JS</Badge>
-                            </div>
-                        </CardHeader>
-                        <CardContent className="-mt-5">
-                            <p className="text-2xl font-semibold">Web Development Course</p>
-                            <div className="flex flex-row justify-between items-center mt-2">
-                                <div className="flex gap-1">
-                                    <Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><StarHalf className="text-apple" size={16} />
-                                </div>
-                                <p className="mt-2">4.7 Rating</p>
-                            </div>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-between items-center">
-                            <p className="text-xl font-bold">$25</p>
-                            <div className="rounded-full p-1 bg-gray-100">
-                                <ArrowRight size={24} className="text-ecstasy" />
-                            </div>
-                        </CardFooter>
-                    </Card>
-                    <Card className="text-dark-ebony">
-                        <CardHeader>
-                            <Image src="https://via.placeholder.com/220x150 " alt="Course image" width={220} height={150} className="mx-auto rounded-xl border border-apple mb-3" />
-                            <div className="flex flex-row justify-center gap-2">
-                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">HTML</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">CSS</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">JS</Badge>
-                            </div>
-                        </CardHeader>
-                        <CardContent className="-mt-5">
-                            <p className="text-2xl font-semibold">Web Development Course</p>
-                            <div className="flex flex-row justify-between items-center mt-2">
-                                <div className="flex gap-1">
-                                    <Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><StarHalf className="text-apple" size={16} />
-                                </div>
-                                <p className="mt-2">4.7 Rating</p>
-                            </div>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-between items-center">
-                            <p className="text-xl font-bold">$25</p>
-                            <div className="rounded-full p-1 bg-gray-100">
-                                <ArrowRight size={24} className="text-ecstasy" />
-                            </div>
-                        </CardFooter>
-                    </Card>
-
+                    ))}
                 </div>
-                <h2 className="text-2xl font-bold tracking-tight -mt-4">All Courses</h2>
-                <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 grid-cols-1 items-center gap-4 pb-4 pt-2">
-                    <Card className="text-dark-ebony">
-                        <CardHeader>
-                            <Image src="https://via.placeholder.com/220x150 " alt="Course image" width={220} height={150} className="mx-auto rounded-xl border border-apple mb-3" />
-                            <div className="flex flex-row justify-center gap-2">
-                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">HTML</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">CSS</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">JS</Badge>
-                            </div>
-                        </CardHeader>
-                        <CardContent className="-mt-5">
-                            <p className="text-2xl font-semibold">Web Development Course</p>
-                            <div className="flex flex-row justify-between items-center mt-2">
-                                <div className="flex gap-1">
-                                    <Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><StarHalf className="text-apple" size={16} />
-                                </div>
-                                <p className="mt-2">4.7 Rating</p>
-                            </div>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-between items-center">
-                            <p className="text-xl font-bold">$25</p>
-                            <div className="rounded-full p-1 bg-gray-100">
-                                <ArrowRight size={24} className="text-ecstasy" />
-                            </div>
-                        </CardFooter>
-                    </Card>
-                    <Card className="text-dark-ebony">
-                        <CardHeader>
-                            <Image src="https://via.placeholder.com/220x150 " alt="Course image" width={220} height={150} className="mx-auto rounded-xl border border-apple mb-3" />
-                            <div className="flex flex-row justify-center gap-2">
-                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">HTML</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">CSS</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">JS</Badge>
-                            </div>
-                        </CardHeader>
-                        <CardContent className="-mt-5">
-                            <p className="text-2xl font-semibold">Web Development Course</p>
-                            <div className="flex flex-row justify-between items-center mt-2">
-                                <div className="flex gap-1">
-                                    <Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><StarHalf className="text-apple" size={16} />
-                                </div>
-                                <p className="mt-2">4.7 Rating</p>
-                            </div>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-between items-center">
-                            <p className="text-xl font-bold">$25</p>
-                            <div className="rounded-full p-1 bg-gray-100">
-                                <ArrowRight size={24} className="text-ecstasy" />
-                            </div>
-                        </CardFooter>
-                    </Card>
-                    <Card className="text-dark-ebony">
-                        <CardHeader>
-                            <Image src="https://via.placeholder.com/220x150 " alt="Course image" width={220} height={150} className="mx-auto rounded-xl border border-apple mb-3" />
-                            <div className="flex flex-row justify-center gap-2">
-                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">HTML</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">CSS</Badge><Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">JS</Badge>
-                            </div>
-                        </CardHeader>
-                        <CardContent className="-mt-5">
-                            <p className="text-2xl font-semibold">Web Development Course</p>
-                            <div className="flex flex-row justify-between items-center mt-2">
-                                <div className="flex gap-1">
-                                    <Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><StarHalf className="text-apple" size={16} />
-                                </div>
-                                <p className="mt-2">4.7 Rating</p>
-                            </div>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-between items-center">
-                            <p className="text-xl font-bold">$25</p>
-                            <div className="rounded-full p-1 bg-gray-100">
-                                <ArrowRight size={24} className="text-ecstasy" />
-                            </div>
-                        </CardFooter>
-                    </Card>
-
-                </div>
-            </div >
+                            </div >
         </>
     )
 }

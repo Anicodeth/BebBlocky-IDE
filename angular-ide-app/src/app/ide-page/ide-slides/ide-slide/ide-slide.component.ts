8039c2b56b5b55d69c1b1c62ca6aef9c02c831a1
ffekirnew <ffekirnew0808@gmail.com>
2023-08-19 22:49:56 +0300

    feat(integrate-ide-to-react-app): Add a new angular app to isolate the ide

---
+++ angular-ide-app/src/app/ide-page/ide-slides/ide-slide/ide-slide.component.ts
@@ -0,0 +1,22 @@
+import { AfterViewChecked, Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-ide-slide',
+  templateUrl: './ide-slide.component.html',
+  styleUrls: ['./ide-slide.component.css']
+})
+export class IdeSlideComponent implements AfterViewChecked {
+  @ViewChild('formattedHtml', { static: false }) formattedHtml!: ElementRef<HTMLIFrameElement>;
+  @Input() title: string = "";
+  @Input() titleFont: string = "";
+  @Input() content: string = "";
+  @Input() contentFont: string = "";
+  @Input() code: string = "";
+  @Input() backgroundColor: string = "";
+  @Input() color: string = "";
+  @Input() image: string = "";
+
+  ngAfterViewChecked() {
+    this.formattedHtml.nativeElement.innerHTML = this.content;
+  }
+}

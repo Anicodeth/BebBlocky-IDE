d5e32edab97d20aacf5735ea96a749eb25778b62
Ananya <afmtrendaid@gmail.com>
2023-08-24 07:20:53 -0700

    all

--- dash_demo-main/dash_demo-main/src/pages/ide.tsx
+++
@@ -1,9 +0,0 @@
-import '../angular-ide-app/main.0ffc7d8829a0a40c'
-import '../angular-ide-app/runtime.1b9c7d0c1fdb4804'
-import '../angular-ide-app/polyfills.5a50205d86725d67'
-
-function AngularIde() {
-  return <angular-ide-app></angular-ide-app>
-}
-
-export default AngularIde

44b69ae33b58eed6bc532886b327eca3f70c57ed
ffekirnew <ffekirnew0808@gmail.com>
2023-08-29 08:03:28 +0300

    feat(ui-completing): Add python logo and implement simple searching functionality

--- dash_demo-main/dash_demo-main/src/pages/courses.tsx
+++ dash_demo-main/dash_demo-main/src/pages/courses.tsx
@@ -1,68 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Head from "next/head";
-import Image from "next/image";
-import { ArrowRight, Star, StarHalf } from "lucide-react";
-
-import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Badge } from "@/components/ui/badge";
 import SearchInput from "@/components/search-input";
-
-import htmlLogo from '../../public/icons/html5/html5-original.svg'
-import cssLogo from '../../public/icons/css3/css3-original.svg'
-import jsLogo from '../../public/icons/javascript/javascript-original.svg'
-interface LogoDict {
-    [key: string]: string; // Assuming the imported SVG paths are strings
-}
-
-const logoDict: LogoDict = {
-    "html": htmlLogo,
-    "css": cssLogo,
-    "js": jsLogo,
-}
-
-interface Slide {
-  _id: string
-  backgroundColor: string
-  color: string
-  title: string
-  titleFont: string
-  content: string
-  contentFont: string
-  code: string
-  startingCode: string
-  image: string
-}
-
-interface Course {
-  _id: number
-  courseId: number
-  courseTitle: string
-  courseDescription: string
-  courseLanguage: string
-  slides: Slide[]
-}
+import useCourses, { Course } from "@/services/useCourses";
+import CourseCard from "@/components/course-card";
 
 export default function CoursesRoute() {
-    const [courses, setCourses] = useState<Course[]>([]);
+  const { courses, isLoading, error } = useCourses();
+  const [ searchTerm, setSearchTerm ] = useState<string>("");
+  const [ filteredCourses, setFilteredCourses ] = useState<Course[]>(courses);
 
-
-    useEffect(() => {
-    fetch("https://beb-blocky-ide.vercel.app/api/v1/courses")
-      .then(response => response.json())
-      .then(data => {
-        setCourses(data.courses)
-        sessionStorage.setItem('auth_token', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGU3NDcwMzI3ZWFkNDhkODFmYTI2ZTciLCJpYXQiOjE2OTI4Nzg3ODl9.JhKUoZLk9U65iIuG_nosAaFnxm56dS_K3jZv00uQUvk" );
-
-      })
-      .catch(error => console.error("Error fetching courses:", error));
-    }, []);
-
+  const handleSearchInputChange = (term: string) => {
+    setSearchTerm(term);
+    setFilteredCourses(courses.filter((course) =>
+      course.courseTitle.toLowerCase().includes(searchTerm.toLowerCase()) || 
+      course.courseLanguage.toLowerCase().includes(searchTerm.toLowerCase()) || 
+      course.courseDescription.toLowerCase().includes(searchTerm.toLowerCase())
+    ));
+  }; 
+    
     return (
         <>
             <Head>
@@ -77,35 +32,15 @@ export default function CoursesRoute() {
                 <p className="text-dark-ebony">
                     Discover and select your preferred course of interest.
                 </p>
-                <SearchInput />
+                <SearchInput onSearchTermChange={handleSearchInputChange} inputPlaceholder={"Search our courses here..."}/>
                 <hr className="w-full border-gray-300 mb-4" />
-        { courses.length == 0 && <p>Sorry, we couldn't load our courses due to errors. Try again later.</p>}
-        { courses.length > 0 && <h2 className="text-2xl font-bold tracking-tight">Most Popular Courses</h2> }
+        { isLoading && <p>Loading...</p> }
+        { error.length > 0 && <p>{ error }</p>}
+        { courses.length > 0 && <h2 className="text-2xl font-bold tracking-tight">{ (searchTerm || "Most Popular") + " Courses" }</h2> }
                 <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 grid-cols-1 items-center gap-4 pb-4 pt-2">
-                { courses.map(course => (
-                    <Card className="text-dark-ebony">
-                        <CardHeader>
-                            <Image src={logoDict[course.courseLanguage]!} alt="Course image" width={180} height={110} className="mx-auto rounded-xl mb-3" />
-                            <div className="flex flex-row justify-center gap-2">
-                                <Badge variant="secondary" className="rounded-xl text-dark-ebony bg-gray-200">{course.courseLanguage.toUpperCase()}</Badge>                            </div>
-                        </CardHeader>
-                        <CardContent className="-mt-5">
-                            <CardTitle>{course.courseTitle}</CardTitle>
-                            <div className="flex flex-row justify-between items-center mt-2">
-                                <div className="flex gap-1">
-                                    <Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><Star size={16} className="text-apple" /><StarHalf className="text-apple" size={16} />
-                                </div>
-                                <p className="mt-2">4.7 Rating</p>
-                            </div>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-between items-center">
-                            <p className="text-xl font-bold">$25</p>
-                            <div className="rounded-full p-1 bg-gray-100">
-                               <a href= {`https://bebblocky.vercel.app/ide/${course.courseId}`}> <ArrowRight size={24} className="text-ecstasy" /> </a>
-                            </div>
-                        </CardFooter>
-                    </Card>
-                    ))}
+                { searchTerm.length > 0 && filteredCourses.map(course => (<CourseCard key={course.courseId} course={course} />))}
+                { searchTerm.length == 0 && courses.map(course => (<CourseCard key={course.courseId} course={course} />))}
+
                 </div>
                             </div >
         </>
